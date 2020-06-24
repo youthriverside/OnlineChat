68883b8bccfa4a249fbf91786c74756aefeb1e00
刘渤源 <63176620+youthriverside@users.noreply.github.com>
2020-06-24 15:35:03 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html> 
+<head>
+<title> 
+在线聊天 
+</title>
+</head> 
+<!-- powered by LiuBoyuan --> 
+<frameset rows="70%,*" BORDER="0"> 
+<frame name="top" src="top.php" marginwidth="0" marginheight="0" scrolling="yes" FRAMEBORDER="NO" noresize> 
+<frame name="bottom" src="input.php" marginwidth="0" marginheight="0" scrolling="no" frameborder="no" noresize> 
+</frameset> 
+<body> 
+</body> 
+</html> 
